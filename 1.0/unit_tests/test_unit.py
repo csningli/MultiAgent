43a769c7de9241b7a40928672317cf321179a5c6
csningli <csningli@gmail.com>
2017-09-01 23:10:57 +0800

    fix test_unit.py.

--- 1.0/unit_tests/test_unit.py
+++ 1.0/unit_tests/test_unit.py
@@ -12,13 +12,14 @@ def test_shape() :
     '''
     >>> test_shape()
     Shape type: CircleShape
-    position: Vec2d(0.0, 0.0)
+    position: (0.0, 0.0)
     angle: 0.0
     range: (10.0, 10.0)
     '''
     shape = CircleShape(Body(10.0, 10.0), 10.0, (0.0, 0.0))
     print("Shape type: %s" % type(shape).__name__)
-    print("position: %s" % shape.get_position())
+    pos = shape.get_position()
+    print("position: (%.1f, %.1f)" % (pos[0], pos[1]))
     print("angle: %s" % shape.get_angle())
     print("range: (%s, %s)" % shape.get_range())
 
@@ -26,12 +27,13 @@ def test_unit() :
     '''
     >>> test_unit()
     Unit name: test_unit
-    position: Vec2d(0.0, 0.0)
+    position: (0.0, 0.0)
     angle: 0.0
     '''
     unit = Unit(name = "test_unit")
     print("Unit name: %s" % unit.name)
-    print("position: %s" % unit.get_position())
+    pos = unit.get_position()
+    print("position: (%.1f, %.1f)" % (pos[0], pos[1]))
     print("angle: %s" % unit.get_angle())
     
 
