933f5e0ace360f2ab9935abc5fcd921b77651d6b
csningli <csningli@gmail.com>
2018-06-11 11:00:01 +0800

    Request tested.

--- 2/2.0/tests/test_request.py
+++ 2/2.0/tests/test_request.py
@@ -9,31 +9,53 @@ sys.path.append("../py")
 
 from multiagent import *
 
-def test_request_init() :
+def test_reqt_basic() :
     '''
-    >>> test_request_init()
+    >>> test_reqt_basic()
+    Initialization.
     Request: <<multiagent.Request content_len=0>>
+    Destinations: []
+    Content:
     '''
+    print("Initialization.")
     reqt = Request()
     print("Request: %s" % reqt.info())
+    print("Destinations: %s" % reqt.dests)
+    print("Content:")
+    for dest, msgs in reqt.content.items() :
+        print("\tDestination: %s " % dest)
+        for msg in msgs :
+            print("\t\tMessage: %s" % msg.info())
 
-def test_request_add() :
+def test_reqt_add_get() :
     '''
-    >>> test_request_add()
+    >>> test_reqt_add_get()
+    Initialization.
     Request: <<multiagent.Request content_len=0>>
-    Add Message: <<multiagent.Message src=src dest=dest key=key value=value>>
-    Request: <<multiagent.Request content_len=1>>
-    Get Messages: ['<<multiagent.Message src=src dest=dest key=key value=value>>']
+    Destinations: ['1']
+    Content:
+    --Destination: 1
+    ----Message: <<multiagent.Message src=0 dest=1 key=key1 value=value1>>
+    ----Message: <<multiagent.Message src=2 dest=1 key=key2 value=value2>>
+    Get Messages to Destinations: 1
+    Message: <<multiagent.Message src=0 dest=1 key=key1 value=value1>>
+    Message: <<multiagent.Message src=2 dest=1 key=key2 value=value2>>
     '''
+    print("Initialization.")
     reqt = Request()
     print("Request: %s" % reqt.info())
-    msg = Message(src = "src", dest = "dest", key = "key", value = "value")
-    print("Add Message: %s" % msg.info())
-    reqt.add_msg(msg)
-    print("Request: %s" % reqt.info())
-    msgs = reqt.get_msgs(dest = "dest")
-    print("Get Messages: %s" % [msg.info() for msg in msgs])
-
+    reqt.add_msg(Message(src = "0", dest = "1", key = "key1", value = "value1"))
+    reqt.add_msg(Message(src = "2", dest = "1", key = "key2", value = "value2"))
+    print("Destinations: %s" % reqt.dests)
+    print("Content:")
+    for dest, msgs in reqt.content.items() :
+        print("--Destination: %s" % dest)
+        for msg in msgs :
+            print("----Message: %s" % msg.info())
+    print("Get Messages to Destinations: 1")
+    msgs = reqt.get_msgs(dest = "1")
+    for msg in msgs :
+        print("Message: %s" % msg.info())
 
 if __name__ == '__main__' :
     result = doctest.testmod()
