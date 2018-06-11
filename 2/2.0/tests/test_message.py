bdd36b0435eac6ab62fbe428531b0038ded9e368
csningli <csningli@gmail.com>
2018-06-11 10:33:28 +0800

    Message tested.

--- 2/2.0/tests/test_message.py
+++ 2/2.0/tests/test_message.py
@@ -9,13 +9,29 @@ sys.path.append("../py")
 
 from multiagent import *
 
-def test_msg() :
+def test_msg_basic() :
     '''
-    >>> test_msg()
-    Message: <<multiagent.Message src=src dest=dest key=key value=value>>
+    >>> test_msg_basic()
+    Initialization.
+    Message: <<multiagent.Message src=0 dest=1 key=key value=value>>
+    Change values of the properties.
+    Source: 1
+    Destination: 0
+    Key: new_key
+    Value: new_value
     '''
-    msg = Message(src = "src", dest = "dest", key = "key", value = "value")
+    print("Initialization.")
+    msg = Message(src = "0", dest = "1", key = "key", value = "value")
     print("Message: %s" % msg.info())
+    print("Change values of the properties.")
+    msg.src = "1"
+    print("Source: %s" % msg.src)
+    msg.dest = "0"
+    print("Destination: %s" % msg.dest)
+    msg.key = "new_key"
+    print("Key: %s" % msg.key)
+    msg.value = "new_value"
+    print("Value: %s" % msg.value)
 
 if __name__ == '__main__' :
     result = doctest.testmod()
