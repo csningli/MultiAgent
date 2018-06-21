38c63963e852f8787d5dd8ce25e98b1bb2ec2f8d
csningli <csningli@gmail.com>
2018-06-22 06:17:41 +0800

    Agent tested.

--- 2/2.0/tests/test_agent.py
+++ 2/2.0/tests/test_agent.py
@@ -13,11 +13,61 @@ from multiagent import *
 def test_agent_basic() :
     '''
     >>> test_agent_basic()
+    Initialization.
     Agent: <<multiagent.Agent name=0 mods_num=1>>
+    Memory: <<multiagent.Memory content_size=1>>
+    Memory Content:
+    Key: name - Value: 0
+    Module: <<multiagent.ObjectModule memory_size=1>>
+    Change values of the properties.
+    Active: False
     '''
     print("Initialization.")
     agent = Agent(name = "0")
     print("Agent: %s" % agent.info())
+    print("Memory: %s" % agent.mem.info())
+    print("Memory Content:")
+    for key, value in agent.mem.content.items() :
+        print("Key: %s - Value: %s" % (key, value))
+    for mod in agent.mods :
+        print("Module: %s" % mod.info())
+    print("Change values of the properties.")
+    agent.active = False
+    print("Active: %r" % agent.active)
+
+def test_agent_memo() :
+    '''
+    >>> test_agent_memo()
+    Initialization.
+    Agent: <<multiagent.Agent name=0 mods_num=1>>
+    Active: True
+    Memory Content:
+    Key: name - Value: 0
+    Change properties by directly apply 'agent.memo = memo', where 'memo' is a map storing the new values.
+    Active: True
+    Memory Content:
+    Key: name - Value: 0
+    '''
+    print("Initialization.")
+    agent = Agent(name = "0")
+    print("Agent: %s" % agent.info())
+    print("Active: %r" % agent.active)
+    print("Memory Content:")
+    for key, value in agent.mem.content.items() :
+        print("Key: %s - Value: %s" % (key, value))
+    print("Change properties by directly apply 'agent.memo = memo', where 'memo' is a map storing the new values.")
+    memo = {
+        "active" : False,
+        "__mem" : {
+            "name" : "0",
+            "key" : True,
+        }
+    }
+    print("Active: %r" % agent.active)
+    print("Memory Content:")
+    for key, value in agent.mem.content.items() :
+        print("Key: %s - Value: %s" % (key, value))
+
 
 if __name__ == '__main__' :
     result = doctest.testmod()
