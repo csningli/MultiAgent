4f4d98571052140bc044c0600ecb0dafbde8e333
csningli <csningli@gmail.com>
2018-08-30 12:16:34 +0800

    example range_detect added.

---
+++ 2/examples/range_detect/range_detect_sim.py
@@ -0,0 +1,107 @@
+
+# MultiAgent 2.0
+# (c) 2017-2018, NiL, csningli@gmail.com
+
+import sys
+
+sys.path.append("../../2.0/")
+
+from mas.multiagent import *
+from mas.utils import *
+
+class DetectObject(Object) :
+    def __init__(self, name, mass = 1.0, radius = 10.0) :
+        super(DetectObject, self).__init__(name, mass, radius)
+        self.__label = self.name
+
+    @property
+    def label(self) :
+        return self.__label
+
+    @label.setter
+    def label(self, value) :
+        self.__label = value
+
+    def draw(self, screen) :
+        super(SteeringObject, self).draw(screen)
+        if self.visible == True :
+            font = pygame.font.Font(None, 16)
+            (width, height) = screen.get_size()
+            pos_draw = (int(width / 2.0 + self.pos[0] - 5.0), int(height / 2.0 - self.pos[1] - self.radius - 10.0))
+            screen.blit(font.render(self.label, 1, THECOLORS["black"]), pos_draw)
+
+class DetectProcessModule(Module) :
+    def process(self) :
+        pos = self.mem.read("pos", None)
+        target = self.mem.read("target", None)
+        if pos is not None and (target is None or ppdist_l2(target, pos) < 5) :
+            self.mem.reg("target", (math.floor(random.random() * 200), math.floor(random.random() * 200)))
+
+class DetectAgent(Agent) :
+    def __init__(self, name) :
+        super(SteeringAgent, self).__init__(name)
+        self.config(mods = [SteeringMoveModule(), SteeringProcessModule()])
+
+    @property
+    def focus(self) :
+        focus_info = {
+        }
+
+        target = self.mem.read("target", None)
+        if target is not None :
+            focus_info["target"] =  "(%4.2f, %4.2f)" % (target[0], target[1])
+
+        pos = self.mem.read("pos", None)
+        if pos is not None :
+            focus_info["pos"] =  "(%4.2f, %4.2f)" % (pos[0], pos[1])
+
+        return focus_info
+
+
+def run_sim(filename = None) :
+    '''
+    run_sim(filename = None)
+    ------------------------
+    filename : the name of the file to save the data; None by default.
+    '''
+
+    # create the oracle space
+
+    oracle = OracleSpace()
+
+    # create the context
+
+    context = Context(oracle = oracle)
+
+    # create the schedule for adding agents in the running
+
+    schedule = Schedule()
+
+    # add objects and agents to the context
+
+    obj = DetectObject(name = "0")
+    obj.pos = (0, 0)
+    context.add_obj(obj)
+    schedule.add_agent(DetectAgent(name = "0"))
+
+    # create the driver
+
+    driver = Driver(context = context, schedule = schedule)
+
+    # create the inspector
+
+    # inspector = Inspector(delay = 10)
+
+    # create the simulator
+
+    sim = Simulator(driver = driver)
+
+    print("Simulating")
+    sim.simulate(graphics = True, filename = filename)
+
+
+if __name__ == '__main__' :
+    filename = None
+    if (len(sys.argv) > 1) :
+        filename = sys.argv[1]
+    run_sim(filename)
